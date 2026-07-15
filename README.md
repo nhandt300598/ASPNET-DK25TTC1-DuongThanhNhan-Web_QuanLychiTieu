3adecaec279ec56f1aaad0ec2cec32ceabd8f1bb
HC-NHANDT22\PC <nhanduong3511@gmail.com>
2026-07-15 08:39:46 +0700

    Cập nhật chuẩn cấu trúc 5 thư mục đồ án

---
+++ README.md
@@ -0,0 +1,9 @@
+# ASPNET-DK25TTC1-DuongThanhNhan-Web_QuanLychiTieu
+
+Đây là đồ án web asp.net
+Đề tài web quản lý chi tiêu cá nhân
+Họ và tên sinh viên: Dương Thành Nhân
+Lớp: DK25TTC1
+MSSV: 170125002
+Email. nhandt300598@tvu-onschool.edu.vn
+SĐT: 0936 418 827
---
+++ scr/Models/GiaoDich.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuanLyChiTieu.Models
+{
+    public class GiaoDich
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Ngày")]
+        public DateTime NgayGiaoDich { get; set; }
+
+        [Display(Name = "Số tiền")]
+        public decimal SoTien { get; set; }
+
+        [Display(Name = "Loại")]    
+        public string LoaiGiaoDich { get; set; } // "Thu" hoặc "Chi"
+
+        [Display(Name = "Danh mục")]
+        public string DanhMuc { get; set; }
+
+        [Display(Name = "Ghi chú")]
+        public string GhiChu { get; set; }
+        public string TaiKhoan { get; set; }
+        public bool LaDinhKy { get; set; }
+    }
+}
